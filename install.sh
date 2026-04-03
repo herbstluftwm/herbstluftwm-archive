b3737b529fe0e3cf5a0c01c33026cbd964b2e307
Thorsten Wißmann <edu@thorsten-wissmann.de>
2026-04-03 10:21:55 +0200

    Install MD5SUMS

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ rsync -rva --exclude '*~' --exclude '.*.swp' --ignore-existing \
     tarballs/ \
     herbstluftwm.org:html/tarballs/
 
+rsync -rva --exclude '*~' \
+    tarballs/MD5SUMS \
+    herbstluftwm.org:html/tarballs/MD5SUMS
+
 rsync -rva --exclude '*~' --exclude '.*.swp' --ignore-existing \
     www/ \
     herbstluftwm.org:html/
